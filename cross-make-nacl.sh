094a620dfbaf6523401f8cbed18cf766727f71a9
Sam Clegg <sbc@google.com>
2012-12-27 20:35:32 +0000

    define NO_SDL_GLEXT

--- cross-make-nacl.sh
+++ cross-make-nacl.sh
@@ -6,8 +6,9 @@
 #  export NACL_GLIBC=1
 #  export NACL_PACKAGES_BITSIZE=64
 #  export NACL_SDK_ROOT=/path/to/pepper_sdk
-#  make regal
 #  make sdl
+#  make nacl-mounts
+#  make regal
 
 if [ -z "$NACL_SDK_ROOT" ]; then
    echo "Set \$NACL_SDK_ROOT to build for NaCl"
@@ -19,6 +20,7 @@ export CXX=x86_64-nacl-g++
 export LD=x86_64-nacl-g++
 export PLATFORM=nacl
 export ARCH=x86_64
+export CFLAGS=-DNO_SDL_GLEXT
 export PKG_CONFIG_PATH=$NACL_SDK_ROOT/toolchain/linux_x86_glibc/x86_64-nacl/usr/lib/pkgconfig
 export PATH=$NACL_SDK_ROOT/toolchain/linux_x86_glibc/bin:$PATH
 
