0be62638bce7504410a7dea4771c17c6ee168b0d
Sam Clegg <sbc@google.com>
2013-01-04 17:02:21 +0000

    add renderer .so files to nmf

--- cross-make-nacl.sh
+++ cross-make-nacl.sh
@@ -30,7 +30,8 @@ make $* || exit $?
 # Create nmf
 EXE=build/release-nacl-$ARCH/ioquake3.$ARCH.nexe
 NMF=build/release-nacl-$ARCH/ioquake3.nmf
-$NACL_SDK_ROOT/tools/create_nmf.py -D $NACL_SDK_ROOT/toolchain/linux_x86_glibc/bin/i686-nacl-objdump $EXE -L $NACL_SDK_ROOT/toolchain/linux_x86_glibc/x86_64-nacl/lib -L $NACL_SDK_ROOT/toolchain/linux_x86_glibc/x86_64-nacl/usr/lib -o $NMF build/release-nacl-$ARCH/renderer_rend2_$ARCH.so -s build/release-nacl-$ARCH || echo "creat_nmf failed"
+SHARED_OBJECTS="build/release-nacl-$ARCH/renderer_rend2_$ARCH.so build/release-nacl-$ARCH/renderer_opengl1_$ARCH.so"
+$NACL_SDK_ROOT/tools/create_nmf.py -D $NACL_SDK_ROOT/toolchain/linux_x86_glibc/bin/i686-nacl-objdump $EXE -L $NACL_SDK_ROOT/toolchain/linux_x86_glibc/x86_64-nacl/lib -L $NACL_SDK_ROOT/toolchain/linux_x86_glibc/x86_64-nacl/usr/lib -o $NMF $SHARED_OBJECTS -s build/release-nacl-$ARCH || echo "creat_nmf failed"
 
 cp misc/index.html build/release-nacl-$ARCH
 
